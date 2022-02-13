eb83c6afc947771f8da8aea12c52c96bbec9cf98
injinjeong <snowwwwww@naver.com>
2022-02-14 06:15:16 +0900

    basic spring batch init

---
+++ src/test/java/io/springbatch/basicspringbatch/BasicspringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.basicspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicspringbatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
